d545935d09b06efc5ffd237e546249669424445c
Leonardo Paz Estevam <estevam.leonardopaz@gmail.com>
2025-09-02 18:26:19 -0300

    fix for colorscheme not loading correctly on startup

--- nvim/config/options.lua
+++ nvim/config/options.lua
@@ -4,3 +4,11 @@
 vim.opt.relativenumber = true
 vim.opt.pumblend = 0
 vim.opt.winblend = 0
+
+-- For some reason, the colorscheme doesn't load correctly on startup, so we force it to reload
+vim.schedule(function()
+	local current_colorscheme = vim.g.colors_name
+	if current_colorscheme == "tokyonight-night" then
+		vim.cmd.colorscheme("tokyonight-night")
+	end
+end)
