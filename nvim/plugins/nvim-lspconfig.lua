0afd3d2b8226d6dd12f23c2e55742dda2019c9aa
Leonardo Paz Estevam <estevam.leonardopaz@gmail.com>
2026-07-16 14:44:50 -0300

    Only texlab for .tex

--- nvim/plugins/nvim-lspconfig.lua
+++ nvim/plugins/nvim-lspconfig.lua
@@ -27,9 +27,7 @@ return {
 				},
 			},
 			texlab = {
-				keys = {
-					{ "<Leader>K", "<plug>(vimtex-doc-package)", desc = "Vimtex Docs", silent = true },
-				},
+				keys = {},
 				settings = {
 					texlab = {
 						auxDirectory = "build",
@@ -48,8 +46,8 @@ return {
 							forwardSearchAfter = true,
 						},
 						forwardSearch = {
-							executable = "zathura",
-							args = { "--synctex-forward", "%l:1:%f", "%p" },
+							executable = "evince",
+							args = { "%p" },
 						},
 						chktex = { onOpenAndSave = true, onEdit = false },
 					},
