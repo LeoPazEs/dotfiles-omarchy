0ee94f47b213d8273be29c8984e714a78125f164
LeoPazEs <estevam.leonardopaz@gmail.com>
2026-08-15 10:20:23 -0300

    migrating input and looknfeel

--- hypr/autostart.lua
+++ hypr/autostart.lua
@@ -1,7 +1,7 @@
 -- Extra autostart processes.
 -- o.launch_on_start("my-service")
 -- If using the Solaar to config the movements in the logitech mouse.
--- o.launch_on_start("solaar -w hide")
+o.launch_on_start("solaar -w hide")
 
 --  o.launch_on_start does not recieve options that is why I am doing like this. I need to set in which monitor I want it executed.
 --  I am using the omarchy-launch-terminal and not the function command_from because the function is local in the helpers.lua!
